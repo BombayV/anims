ca9d1425eda85f172fa9490a87ecc6027181507b
BombayV <OMGmsebastian@gmail.com>
2021-12-03 18:12:55 -0500

    feat(js): added attributes

--- html/js/modules/fetch.js
+++ html/js/modules/fetch.js
@@ -1,3 +1,5 @@
+// All favorites will be sotred in favoriteAnims
+
 const doc = document;
 
 export const fetchNUI = async (cbname, data) => {
@@ -15,16 +17,19 @@ export const fetchNUI = async (cbname, data) => {
 const setText = (elem, text) => elem.textContent = text;
 
 const getFavorite = (elemId) => {
-    const exists = false;
+    let exists = false;
     if (elemId) {
-        const id = localStorage.getItem(elemId);
-        (id) ? exists = true : exist = false;
+        const id = JSON.parse(localStorage.getItem('favoriteAnims'));
+        for (let i = 0; i < id.length; i++) {
+            if (id[i] == elemId) {
+                exists = true
+            }
+        }
     }
     return exists;
 }
 
 export const createPanels = (panelData) => {
-    console.log(panelData)
     const main = doc.getElementById('anims-holder');
     panelData.forEach(panel => {
         if (panel && panel.type) {
@@ -38,9 +43,30 @@ export const createPanels = (panelData) => {
             star.classList.add('material-icons', 'star');
             star.textContent = 'star';
 
+            block.id = panel.id
+            block.setAttribute('data-dances', (panel.dances) ? (JSON.stringify({dict: panel.dances.dict, anim: panel.dances.anim})) : null);
+            block.setAttribute('data-scenarios', (panel.scenarios) ? (JSON.stringify({sex: panel.scenarios.sex, scene: panel.scenarios.scene})) : b);
+            block.setAttribute('data-expressions', (panel.expressions) ? (JSON.stringify({expressions: panel.expressions.expression})) : b);
+            block.setAttribute('data-walks', (panel.walks) ? (JSON.stringify({style: panel.walks.style})) : b);
+            block.setAttribute('data-props', (panel.props) ? (JSON.stringify({prop: panel.props.prop, propBone: panel.props.propBone, propPlacement: panel.props.propPlacement, propTwo: panel.props.propTwo || null, propTwoBone: panel.props.propTwoBone || null, propTwoPlacement: panel.props.propTwoPlacement || null})): null);
+            block.setAttribute('data-particles', (panel.particles) ? (JSON.stringify({asset: panel.particles.asset, name: panel.particles.name, placement: panel.particles.placement})) : null);
+
             star.addEventListener('click', e => {
-                const isSaved =
-                e.target.style.color = 'yellow';
+                const isSaved = getFavorite(block.id);
+                const favs = JSON.parse(localStorage.getItem('favoriteAnims'));
+                if (isSaved) {
+                    e.target.style.color = '#999999';
+                    for (let i = 0; i < favs.length; i++) {
+                        if (favs[i] == block.id) {
+                            favs.splice(i, 1);
+                            localStorage.setItem('favoriteAnims', JSON.stringify(favs))
+                        }
+                    }
+                } else {
+                    favs.push(block.id)
+                    localStorage.setItem('favoriteAnims', JSON.stringify(favs))
+                    e.target.style.color = '#f7ca17';
+                }
             });
 
             setText(title, panel.title);
--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,11 @@
 import { createPanels } from "./modules/fetch.js";
 
 window.addEventListener('load', () => {
+    window.localStorage.clear()
+    if (localStorage.getItem('favoriteAnims') == null) {
+        localStorage.setItem('favoriteAnims', JSON.stringify([]))
+    }
+
     fetch('../anims.json')
     .then((resp) => resp.json())
     .then((data) => createPanels(data))
