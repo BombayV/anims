8e608a58e444b27ebfd01544e6d539eaa11d35de
BombayV <OMGmsebastian@gmail.com>
2021-12-20 01:56:00 -0500

    feat(update): actual update data

--- server/update.lua
+++ server/update.lua
@@ -1,14 +1,20 @@
-local function checkVersion(e, latest, headers)
+local function checkVersion(e, latest, _)
     local versionData = json.decode(LoadResourceFile('anims', 'update.json'))
-    print(e)
-    print(latest)
-    print(headers)
+    local latest = json.decode(latest)
+    if e ~= 200 then
+        return print('Error: Anims could not verify with Github')
+    end
     if versionData then
         local version = versionData.version
         if version ~= latest.version and version < latest.version then
-            print('^2Anims are outdated!\n^1Get your update from https://github.com/BombayV/anims')
+            print('^1Anims are outdated!')
+            print('^3What is new: ^7' .. latest.new)
+            print('^3Necessary?: ^7' .. latest.update)
+            print('^2Get your update from: ^7https://github.com/BombayV/anims')
+        elseif version > latest.version then
+            print('^3Your version is above latest. Sus stuff you got here.^7')
         else
-            print('^2Anims are updated!')
+            print('^2Anims are updated!^7')
         end
     else
         print('You removed the JSON file smh. Goodbye to updates :sadge:')
@@ -18,7 +24,7 @@ end
 local function updateStatus()
     SetTimeout(10 * 60000, updateStatus)
 
-    PerformHttpRequest('https://raw.githubusercontent.com/BombayV/anims/main/version.json', checkVersion, "GET")
+    PerformHttpRequest('https://raw.githubusercontent.com/BombayV/anims/main/update.json', checkVersion, "GET")
 end
 
 updateStatus()
