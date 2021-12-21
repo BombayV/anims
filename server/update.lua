46464eb7ddba016742fba5ec422d0d8ec0a778e8
BombayV <OMGmsebastian@gmail.com>
2021-12-20 23:57:31 -0500

    feat(anims): new sex anims

--- server/update.lua
+++ server/update.lua
@@ -1,5 +1,7 @@
+local firstTime = false
+local versionData = json.decode(LoadResourceFile('anims', 'update.json'))
+
 local function checkVersion(e, latest, _)
-    local versionData = json.decode(LoadResourceFile('anims', 'update.json'))
     local latest = json.decode(latest)
     if e ~= 200 then
         return print('Error: Anims could not verify with Github')
@@ -14,7 +16,10 @@ local function checkVersion(e, latest, _)
         elseif version > latest.version then
             print('^3Your version is above latest. Sus stuff you got here.^7')
         else
-            print('^2Anims are updated!^7')
+            if not firstTime then
+                firstTime = true
+                print('^2Anims are updated!^7')
+            end
         end
     else
         print('You removed the JSON file smh. Goodbye to updates :sadge:')
@@ -22,7 +27,7 @@ local function checkVersion(e, latest, _)
 end
 
 local function updateStatus()
-    SetTimeout(10 * 60000, updateStatus)
+    SetTimeout(1000 * 60 * 30, updateStatus)
 
     PerformHttpRequest('https://raw.githubusercontent.com/BombayV/anims/main/update.json', checkVersion, "GET")
 end
